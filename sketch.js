52dca5f5d62b366e17e034234d1a7d4071bf822e
Mathz333 <matheusgarro57@gmail.com>
2024-06-11 15:53:59 -0300

    adicionando arquivos

--- sketch.js
+++ sketch.js
@@ -1,1 +1,24 @@
+//Vida de  inseto. Livre, animação
+//divertidamente, Livre, animação, aventura
+//furiosa: Um Saga Mad Max 16, ação
 
+
+
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+    let recomendacao = geraRecomendacao(idade);
+    text(recomendacao, width / 2, height / 2);
+  }
+  
+  function geraRecomendacao(idade);
+    if (idade >= 10) {
+     return "Vida de Inseto";
+    } else {
+      return "divertidamente";
+    }
+    }
+    
